ef8f9dc1519001a7925319f881c3233e9d44b321
Alok-Kumar-1 <alokkumar68338@gmail.com>
2026-06-24 14:24:06 +0530

    Initial commit: Node.js application with Docker

---
+++ application/src/server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Production CI/CD Pipeline Project",
+    status: "running"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy",
+    environment: process.env.NODE_ENV
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
